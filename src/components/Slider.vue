091f2887365ce623ae44b8cb5423ee6cf63675c6
lornamicheller <37231071+lornamicheller@users.noreply.github.com>
2019-06-27 08:45:43 -0400

    commit change costa rica photo

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -24,7 +24,7 @@
                     #slider.clearfix
                         #camera_wrap
                             div(data-src='/es/static/images/tripmagic.jpg')
-                            div(data-src='/es/static/images/michael.png')
+                            //- div(data-src='/es/static/images/michael.png')
                             //- div(data-src='/es/static/images/moneyclub.jpg')
                             //- div(data-src='/es/static/images/slide05.jpg')
                             //- test
